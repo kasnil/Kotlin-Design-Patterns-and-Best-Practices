8197da61354a0b2f59c494c3aa58fd7828469ade
kasnil <kasnil@outlook.ie>
2025-07-13 17:34:47 +0300

    Static Factory Method

---
+++ Static Factory Method/Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+    Server.withPort(8080)
+}
+
+class Server private constructor(
+    port: Long,
+) {
+    init {
+        println("Server started on port $port")
+    }
+
+    companion object {
+        fun withPort(port: Long) = Server(port)
+    }
+}
